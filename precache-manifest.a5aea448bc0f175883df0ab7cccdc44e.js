d8d3075a6945abc1cdbf64b4889b51eee5408238
Usmanqayyum109 <you@example.com>
2022-12-03 23:50:33 +0500

    Updates

---
+++ precache-manifest.a5aea448bc0f175883df0ab7cccdc44e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e28db6a707dcbb91271",
+    "url": "/Mathematics-app/static/css/main.58d1039e.chunk.css"
+  },
+  {
+    "revision": "1e28db6a707dcbb91271",
+    "url": "/Mathematics-app/static/js/main.1e28db6a.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/Mathematics-app/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "c9e04fcbb5602c088617",
+    "url": "/Mathematics-app/static/js/2.c9e04fcb.chunk.js"
+  },
+  {
+    "revision": "2c6bb2a6ec777744ec2c",
+    "url": "/Mathematics-app/static/js/runtime~main.2c6bb2a6.js"
+  },
+  {
+    "revision": "5e26465fa4f8fbfe0752c6d0a63c89c3",
+    "url": "/Mathematics-app/index.html"
+  }
+];
